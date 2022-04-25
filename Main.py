d03fcabad6a8d225e93c95696bd5b729d75d8cd2
Ekky Armandi <ekkyarmandi@gmail.com>
2022-04-25 14:55:57 +0700

    adding login feature to the main script

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 from Admin import Admin
 from User import User
-
+from Course import Course
+from Instructor import Instructor
+from Student import Student
+from Review import Review
 
 def show_menu():
     pass
@@ -19,19 +22,44 @@ if __name__ == "__main__":
     # print a welcome message
     print('Welcome to our system')
 
-    # manually register admin
-    while True:
-        user_input = input('Please input username and password to login: (format username password)\n')
-        if user_input == 'logout':
-            print('Thank you for using our system')
-        else:
-            try:
-                username = user_input.split(" ")[0]
-                password = user_input.split(" ")[1]
-                if username == 'admin' and password == 'admin':
-                    admin = Admin(username=username,password=password)
-                    admin.register_admin()
-                    print(admin)
-                    break
-            except:
-                print('username or password incorrect')
+    # ask user to login
+    raw_input = input('Pleaes input username and password to login: (format username password)\n').strip().split(" ")
+    if len(raw_input) > 1:
+        username = raw_input[0]
+        password = raw_input[1]
+    elif len(raw_input) <= 1:
+        username = raw_input[0]
+        password = ""
+
+    user = User()
+    result = user.login(username,password)
+    print(result)
+
+    if result[1] == 'Admin':
+        user = Admin(
+            id=result[2].split(";;;")[0],
+            username=result[2].split(";;;")[1],
+            password=result[2].split(";;;")[2]
+        )
+
+    elif result[1] == 'Instructor':
+        user = Instructor(
+            id=result[2].split(";;;")[0],
+            username=result[2].split(";;;")[1],
+            password=result[2].split(";;;")[2],
+            display_name=result[2].split(";;;")[3],
+            job_title=result[2].split(";;;")[4],
+            image_100x100=result[2].split(";;;")[5],
+            course_id_list=result[2].split(";;;")[6].split('--')
+        )
+
+    elif result[1] == 'Student':
+        user = Student(
+            id=result[2].split(";;;")[0],
+            username=result[2].split(";;;")[1],
+            password=result[2].split(";;;")[2],
+            title=result[2].split(";;;")[3],
+            image_50x50=result[2].split(";;;")[4],
+            initials=result[2].split(";;;")[5],
+            review_id=result[2].split(";;;")[6]
+        )
